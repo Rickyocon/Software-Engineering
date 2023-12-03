9fe943d87a16f07b96099a133016b24ec10fb2ad
Doraemon-star <73323137+Doraemon-star@users.noreply.github.com>
2023-12-03 15:41:27 -0500

    Delete virus_count.py

--- virus_count.py
+++
@@ -1,9 +0,0 @@
-
-def virus_count(lPath):
-    with open(lPath, 'r') as file:
-        lines = file.readlines()
-
-    target_line = [line for line in lines if "Infected files:" in line][-1]
-    virus_count = int(target_line.split(":")[-1].strip())
-    
-    return virus_count
