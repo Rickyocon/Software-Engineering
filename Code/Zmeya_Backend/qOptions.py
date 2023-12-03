c603b0ed0d3d50e3328efcfaa5db64f6f154e073
Ricky O'Connor <94875214+Rickyocon@users.noreply.github.com>
2023-12-03 14:59:57 -0800

    Add files via upload

---
+++ Code/Zmeya_Backend/qOptions.py
@@ -0,0 +1,19 @@
+from Zmeya_Backend.core import path_format, customPath
+import shutil
+import os
+
+#Delete most recent quarantine folder when users choose to delete.
+def delete(qPath):
+    if qPath:
+        qPath = path_format(qPath)
+    else:
+        qfolder = "Zmeya_quarantine"
+        qPath = customPath(qfolder)
+
+    qFolders = [os.path.join(qPath, folder) for folder in os.listdir(qPath) if os.path.isdir(os.path.join(qPath, folder))]
+    if qFolders:
+        target_qFolder = max(qFolders, key=os.path.getctime)
+        shutil.rmtree(target_qFolder)
+        print("Infected files were deleted successfully!")
+    else:
+        print("Nothing to delete.")
