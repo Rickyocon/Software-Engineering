640c52b2d18816f4734ab9bafc38cf79f13089b3
Doraemon-star <73323137+Doraemon-star@users.noreply.github.com>
2023-12-05 11:34:05 -0500

    Update home.py

--- Code/UI/home.py
+++ Code/UI/home.py
@@ -11,7 +11,7 @@ def customPath(dirName):
 def display_home_content(main_frame):
     for widget in main_frame.winfo_children():
         widget.destroy()
-    logo = tk.PhotoImage(file=customPath("UI\\assets\\Zmeya.png"))
+    logo = tk.PhotoImage(file=customPath("assets\\Zmeya.png"))
     logo_label = tk.Label(main_frame, image=logo, bg=style.BACKGROUND_COLOR)
     welcome_label = tk.Label(main_frame, text="Welcome to Zmeya", **style.LABEL_STYLES)
 
