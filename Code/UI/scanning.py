1f4b004137d5cc6d60d7161992a02c12054fd753
Anna K <105675374+krimebae@users.noreply.github.com>
2023-12-05 14:06:48 -0500

    Update scanning.py

--- Code/UI/scanning.py
+++ Code/UI/scanning.py
@@ -145,109 +145,97 @@ def display_schedule_scan_options(main_frame, update_main_frame, clear_main_fram
     title_label = tk.Label(main_frame, text="Schedule Scan", **style.LABEL_STYLES)
     title_label.pack(pady=style.PAD_Y)
 
-    # Schedule Type Selection Frame
-    lbl_schedule_type = tk.Label(main_frame, text="Select Schedule Type:", **style.LABEL_STYLES)
+    # Schedule Type Selection
+    lbl_schedule_type = tk.Label(main_frame, text="Schedule Type:", **style.LABEL_STYLES)
     lbl_schedule_type.pack(pady=style.PAD_Y)
-    schedule_type_var = tk.StringVar(main_frame)
-    schedule_type_var.set('once')
-    schedule_types = ["daily", "weekly", "monthly", "once"]
-  #  schedule_type_var = tk.StringVar(main_frame)
-    schedule_type_menu = tk.OptionMenu(main_frame, schedule_type_var, *schedule_types)
+    schedule_type_var = StringVar(main_frame)
+    schedule_types = ["Once", "Daily", "Weekly", "Monthly"]
+    schedule_type_menu = OptionMenu(main_frame, schedule_type_var, *schedule_types)
     schedule_type_menu.pack(pady=style.PAD_Y)
 
     # Drive/Folder Selection Frame
-    frame_scan_target = Frame(main_frame)
-    frame_scan_target.pack(pady=style.PAD_Y)
-    lbl_scan_target = tk.Label(frame_scan_target, text="Select Drive/Folder to Scan:", **style.LABEL_STYLES)
-    lbl_scan_target.pack(side=tk.LEFT)
-    entry_scan_target = tk.Entry(frame_scan_target, **style.ENTRY_STYLES)
-    entry_scan_target.pack(side=tk.LEFT)
-    btn_browse = tk.Button(frame_scan_target, text="Browse", command=lambda: browse_target(entry_scan_target), **style.BUTTON_STYLES)
-    btn_browse.pack(side=tk.LEFT)
+    lbl_scan_target = tk.Label(main_frame, text="Select Drive/Folder to Scan:", **style.LABEL_STYLES)
+    lbl_scan_target.pack(pady=style.PAD_Y)
+    entry_scan_target = tk.Entry(main_frame, **style.ENTRY_STYLES)
+    entry_scan_target.pack(pady=style.PAD_Y)
+    btn_browse = tk.Button(main_frame, text="Browse", command=lambda: browse_target(entry_scan_target), **style.BUTTON_STYLES)
+    btn_browse.pack(pady=style.PAD_Y)
 
     def browse_target(entry_widget):
         folder_selected = filedialog.askdirectory()
         entry_widget.delete(0, tk.END)
         entry_widget.insert(0, folder_selected)
 
-    # Calendar for selecting date
+    # Date Selection for 'Once' schedule type
     lbl_calendar = tk.Label(main_frame, text="Select Date:", **style.LABEL_STYLES)
     lbl_calendar.pack(pady=style.PAD_Y)
-    calendar = DateEntry(main_frame, width=12, background='darkblue', foreground='white', borderwidth=2)
+    calendar = DateEntry(main_frame, **style.ENTRY_STYLES)
     calendar.pack(pady=style.PAD_Y)
 
-    # Time Selection Widgets
+    # Time Selection
     lbl_time = tk.Label(main_frame, text="Select Time (HH:MM):", **style.LABEL_STYLES)
     lbl_time.pack(pady=style.PAD_Y)
-    hours = [f"{h:02d}" for h in range(1, 13)]
-    minutes = [f"{m:02d}" for m in range(60)]
-    am_pm = ["AM", "PM"]
-    hour_var = tk.StringVar(main_frame)
-    minute_var = tk.StringVar(main_frame)
-    am_pm_var = tk.StringVar(main_frame)
-    hour_menu = tk.OptionMenu(main_frame, hour_var, *hours)
-    minute_menu = tk.OptionMenu(main_frame, minute_var, *minutes)
-    am_pm_menu = tk.OptionMenu(main_frame, am_pm_var, *am_pm)
+    hour_var = StringVar(main_frame)
+    hour_menu = OptionMenu(main_frame, hour_var, *[f"{i:02d}" for i in range(24)])
     hour_menu.pack(pady=style.PAD_Y)
+    minute_var = StringVar(main_frame)
+    minute_menu = OptionMenu(main_frame, minute_var, *[f"{i:02d}" for i in range(60)])
     minute_menu.pack(pady=style.PAD_Y)
-    am_pm_menu.pack(pady=style.PAD_Y)
 
-    # Additional Parameters Frame
-    lbl_extra_param = tk.Label(main_frame, text="Day (optional)", **style.LABEL_STYLES)
+    # Additional Parameter for Weekly or Monthly schedule types
+    lbl_extra_param = tk.Label(main_frame, text="Day (for Weekly or Monthly):", **style.LABEL_STYLES)
     lbl_extra_param.pack(pady=style.PAD_Y)
-    extra_param_var = tk.StringVar
-    extra_param_var.menu = tk.OptionMenu(main_frame, extra_param_var, *[])
-    extra_param_var.menu.pack(pady=style.PAD_Y)
-    # Schedule Button
-    btn_schedule = tk.Button(main_frame, text="Schedule Scan", command=lambda: schedule_scan(calendar.get_date(), hour_var.get(), minute_var.get(), am_pm_var.get(), entry_scan_target.get()), **style.BUTTON_STYLES)
-    btn_schedule.pack(pady=style.PAD_Y)
-
-    extra_param_var = tk.StringVar(main_frame)
-    extra_param_menu = tk.OptionMenu(main_frame, extra_param_var, ())
+    extra_param_var = StringVar(main_frame)
+    extra_param_menu = OptionMenu(main_frame, extra_param_var, ())
     extra_param_menu.pack(pady=style.PAD_Y)
 
     def update_extra_param_options(*args):
         schedule_type = schedule_type_var.get()
-        if schedule_type == 'weekly':
-            days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-            extra_param_menu['menu'].delete(0, "end")
+        if schedule_type == "Weekly":
+            days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+            extra_param_var.set(days_of_week[0])  # Default to Monday
+            extra_param_menu["menu"].delete(0, "end")
             for day in days_of_week:
-                extra_param_menu['menu'].add_command(label=day, command=tk._setit(extra_param_var, day))
-            lbl_extra_param.config(text="Day of week:")
-        elif schedule_type == 'monthly':
-        
-            days_of_month = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-            extra_param_menu['menu'].delete(0, "end")
+                extra_param_menu["menu"].add_command(label=day, command=lambda value=day: extra_param_var.set(value))
+        elif schedule_type == "Monthly":
+            days_of_month = [str(day) for day in range(1, 32)]
+            extra_param_var.set(days_of_month[0])  # Default to the 1st
+            extra_param_menu["menu"].delete(0, "end")
             for day in days_of_month:
-                extra_param_menu['menu'].add_command(label=day, command=tk._setit(extra_param_var, day))
-            lbl_extra_param.config(text="Day of month:")
+                extra_param_menu["menu"].add_command(label=day, command=lambda value=day: extra_param_var.set(value))
         else:
-            extra_param_menu['menu'].delete(0, "end")
-            lbl_extra_param.config(text="Day (optional):")
-                      
+            extra_param_menu["menu"].delete(0, "end")
+
     schedule_type_var.trace('w', update_extra_param_options)
 
-    def schedule_scan(schedule_type, date, hour, minute, scan_path, extra_param=None):
+    def schedule_scan():
+        scan_path = entry_scan_target.get()
+        schedule_type = schedule_type_var.get()
+         date = calendar.get_date()
+        hour = hour_var.get()
+        minute = minute_var.get()
+        extra_param = extra_param_var.get() if extra_param_var.get() else None
+
         try:
-            year, month, day = [int(x) for x in date.split('-')]
-            hour_24=int(hour)
-
-            if schedule_type == "daily":
-                core.schedule_scan_daily(hour_24, int(minute), scan_path)
-            elif schedule_type == "weekly":
-                core.schedule_scan_weekly(extra_param, hour_24, int(minute), scan_path)
-            elif schedule_type == "monthly":
-                core.schedule_scan_monthly(extra_param, hour_24, int(minute), scan_path)
-            elif schedule_type == "once":
-                core.schedule_scan_once(year, month, day, int(hour), int(minute), scan_path)
-            else:
-                raise ValueError
-        
-            update_main_frame('Scan scheduled at {hour}:{minute} {am_pm} on {date} for scan_path')
+            if schedule_type == 'Once':
+                year, month, day = [int(x) for x in date.split('-')]
+                schedule_scan_once(year, month, day, hour, minute, scan_path)
+            elif schedule_type == 'Daily':
+                schedule_scan_daily(hour, minute, scan_path)
+            elif schedule_type == 'Weekly':
+                if not extra_param:
+                    raise ValueError("Please select a day of the week.")
+                schedule_scan_weekly(extra_param, hour, minute, scan_path)
+            elif schedule_type == 'Monthly':
+                if not extra_param:
+                    raise ValueError("Please select a day of the month.")
+                schedule_scan_monthly(extra_param, hour, minute, scan_path)
+            update_main_frame(f"Scan scheduled: {schedule_type} at {hour}:{minute} for path {scan_path}")
         except Exception as e:
-            update_main_frame("Error scheduling scan: {str(e)}")
-        
+            update_main_frame(f"Error scheduling scan: {e}")
 
+    btn_schedule = tk.Button(main_frame, text="Schedule Scan", command=schedule_scan, **style.BUTTON_STYLES)
+    btn_schedule.pack(pady=style.PAD_Y)
     
     # ... Additional code if needed ...
 ("Scan scheduled  ")
