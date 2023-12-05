9e6ae271a732a18ae87a424b1212669c3c95b9b4
Anna K <105675374+krimebae@users.noreply.github.com>
2023-12-04 22:20:42 -0500

    Update scheduler.py

--- Code/Zmeya_Backend/scheduler.py
+++ Code/Zmeya_Backend/scheduler.py
@@ -10,18 +10,9 @@ def main():
     # Use customPath function from core.py
     scan_path = core.customPath(dirName)
     
-    #or 
-    #scan_path = core.sPaths
-    
-    # Define log and quarantine paths
-    log_path = core.customPath("Zmeya_log")
-    quarantine_path = core.customPath("Zmeya_quarantine")
-
-    
     #full scan : core.customScan(drives, log_path, quarantine_path)
     # Perform the custom scan
     core.customScan([scan_path], log_path, quarantine_path) # for custompath(dirName)
-    #  core.customScan(scan_paths, log_path, quarantine_path) #for sPaths
 
 if __name__ == "__main__":
     main()
